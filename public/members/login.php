9c7bb8b33ff2eebd834cf484afffc837a8179266
dvalverderuiz <davidvalverderuiz209@gmail.com>
2024-11-01 12:30:03 +0100

    index images + index content

--- public/members/login.php
+++ public/members/login.php
@@ -18,17 +18,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Protegerse contra inyecciones SQL usando consultas preparadas
     $sql = "SELECT * FROM usuarios WHERE email = ? AND contraseña = ?";
     $stmt = $conn->prepare($sql);
-
     $stmt->bind_param("ss", $email_login, $contraseña_login);
-
-   
     $stmt->execute();
 
-    
     $result = $stmt->get_result();
+    /* 
+    $sql2 = "SELECT..."
+    $stmt2 = $conn->prepare($sql2);
+    $stmt2->execute();
+    */
+
 
     if ($result->num_rows > 0) {
         echo "Usuario y Contraseña correctos. Inicio de sesión exitoso.";
+        
     } else {
         echo "Correo electrónico o contraseña incorrectos.";
     }
