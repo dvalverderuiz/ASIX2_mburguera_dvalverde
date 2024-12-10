17203393be3b3c49486ea83804e8875dc9124bc0
David Valverde Ruiz <89070011+dvalverderuiz@users.noreply.github.com>
2024-12-10 16:26:31 +0100

    Update conexion.php

--- public/conexion.php
+++ public/conexion.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "10.20.30.110";
+$username = "davidmiquel";
+$password = "p4ssw0rd_88";
 $dbname = "conexion";
 
 
