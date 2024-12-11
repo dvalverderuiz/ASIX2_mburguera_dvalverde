ad37db6ec4c28675e812039229a44b650c76ef07
David Valverde Ruiz <89070011+dvalverderuiz@users.noreply.github.com>
2024-12-11 16:22:41 +0100

    fix conexion.php

--- public/conexion.php
+++ public/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "10.20.30.110";
 $username = "davidmiquel";
-$password = "p4ssw0rd_88";
+$password = "P4ssw0rd_88";
 $dbname = "conexion";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
